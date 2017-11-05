ba39c2a858cb251218af81ef68248995bebb250f
trickstertrick <14nl40@queensu.ca>
2017-11-05 16:38:08 -0500

    Removed unused resources

--- src/cpp/controllers/FaceCropperController.cpp
+++ src/cpp/controllers/FaceCropperController.cpp
@@ -7,8 +7,6 @@
 #include "opencv2/core.hpp"
 #include "opencv2/objdetect.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-//#include "opencv/highgui.h"
-//#include "opencv2/highgui/highgui.hpp"
 
 using namespace std;
 using namespace cv;
