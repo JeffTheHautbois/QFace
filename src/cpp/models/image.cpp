e352c28e7a77bc8aeccca8bf96151e7d542386c1
trickstertrick <14nl40@queensu.ca>
2017-11-06 13:43:39 -0500

    Added blank constructors

--- src/cpp/models/image.cpp
+++ src/cpp/models/image.cpp
@@ -2,11 +2,22 @@
 #include <vector>
 #include "Image.h"
 #include "MatModel.h"
-
+// Constructor given cv::Mat
 Image::Image(cv::Mat matrix){
   imageData = matrix;
 }
 
+// Constructor given base64 string
+Image::Image(std::string base64EncodedImage){
+
+}
+
+// Constructor given byte vector
+Image::Image(std::vector<unsigned char> imageBytes){
+
+}
+
+// Data accessor
 cv::Mat Image::getData() const{
   return imageData;
 }
