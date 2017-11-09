b6c12b8b7036e07b5a02300a153584eefedbd59d
andusy <andus401@gmail.com>
2017-11-09 16:05:32 -0500

    Image class comments

--- src/cpp/models/Image.h
+++ src/cpp/models/Image.h
@@ -7,16 +7,16 @@
 
 class Image {
 public:
-  Image(cv::Mat matrix);
-  Image(std::string base64EncodedImage);
-  Image(std::vector<unsigned char> imageBytes);
+	// Image constructors
+	Image(cv::Mat matrix);
+	Image(std::string base64EncodedImage);
+	Image(std::vector<unsigned char> imageBytes);
 
-  cv::Mat asMat() const; // accessor
-  std::string asBase64();
-  std::vector<unsigned char> asBytes();
-
+	cv::Mat asMat() const; // Get the image as a matrix
+	std::string asBase64(); // Get the image as a base64 string
+	std::vector<unsigned char> asBytes(); // Get the image as bytes
 private:
-  cv::Mat imageData; // the image stored as a Mat
+	cv::Mat imageData; // the image stored as a Mat
 };
 
 #endif /* SRC_CPP_MODELS_IMAGE_H_ */
--- src/cpp/models/image.cpp
+++ src/cpp/models/image.cpp
@@ -13,31 +13,34 @@ Image::Image(cv::Mat matrix) : imageData(matrix){
 
 // Constructor given base64 string
 Image::Image(std::string base64EncodedImage){
-	std::vector<unsigned char> imageBytes = base64_decode(base64EncodedImage);
-  cv::Mat data_mat(imageBytes,true);
-  imageData = data_mat;
+	std::vector<unsigned char> imageBytes = base64_decode(base64EncodedImage); // Decodes the given base64 string to a vector of unsigned chars
+	cv::Mat data_mat(imageBytes,true); // Create an opencv matrix object from the vector of unsigned chars
+	imageData = data_mat;
 }
 
 // Constructor given byte vector
 Image::Image(std::vector<unsigned char> imageBytes){
-  cv::Mat data_mat(imageBytes,true);
-  imageData = data_mat;
+	cv::Mat data_mat(imageBytes,true); // opencv constructor for a matrix
+	imageData = data_mat; // Set imageData to the matrix created above
 }
 
-// Data accessor
+// Returns the image as a matrix
 cv::Mat Image::asMat() const{
-  return imageData;
+	return imageData;
 }
 
-std::vector<unsigned char> Image::asBytes(){
-  std::vector<unsigned char> imageBytes;
-  convertMatToByteVector(imageData,&imageBytes);
-  return imageBytes;
-}
-
+// Returns the image as a base 64 string
 std::string Image::asBase64(){
-  std::vector<unsigned char> imageBytes;
-  convertMatToByteVector(imageData,&imageBytes);
-  std::string encodedData = base64_encode(&imageBytes[0], imageBytes.size());
-  return encodedData;
+	std::vector<unsigned char> imageBytes; // Vector variable declaration
+  	convertMatToByteVector(imageData,&imageBytes); // Converts the imageData matrix to a vector and stores it into imageBytes
+  	std::string encodedData = base64_encode(&imageBytes[0], imageBytes.size()); // Encodes the vector to a base64 string
+  	return encodedData; // Return the encoded string
 }
+
+// Returns the image as a vector of unsifned chars (bytes)
+std::vector<unsigned char> Image::asBytes(){
+	std::vector<unsigned char> imageBytes; // Vector variable declaration
+	convertMatToByteVector(imageData,&imageBytes); // Converts the imageData matrix to a vector and stores it into imageBytes
+	return imageBytes; // Return the vector
+}
+
