9901056c4370f04bccf97b99821e0dd84df041e7
trickstertrick <14nl40@queensu.ca>
2017-11-12 12:44:46 -0500

    Attempting to figure out implementation of FaceRecognizer of OpenCv

--- src/cpp/controllers/FacialRecognitionController.h
+++ src/cpp/controllers/FacialRecognitionController.h
@@ -4,11 +4,55 @@
 #include <vector>
 #include "opencv2/core.hpp"
 #include "Image.h"
+#include <string>
+#include <vector>
+
+using namespace cv;
+
+class FaceRecognizer : public Algorithm
+{
+public:
+    //! virtual destructor
+    virtual ~FaceRecognizer() {}
+
+    // Trains a FaceRecognizer.
+    virtual void train(InputArray src, InputArray labels) = 0;
+
+    // Updates a FaceRecognizer.
+    virtual void update(InputArrayOfArrays src, InputArray labels);
+
+    // Gets a prediction from a FaceRecognizer.
+    virtual int predict(InputArray src) const = 0;
+
+    // Predicts the label and confidence for a given sample.
+    virtual void predict(InputArray src, int &label, double &confidence) const = 0;
+
+    // Serializes this object to a given filename.
+    virtual void save(const String& filename) const;
+
+    // Deserializes this object from a given filename.
+    virtual void load(const String& filename);
+
+    // Serializes this object to a given cv::FileStorage.
+    virtual void save(FileStorage& fs) const = 0;
+
+    // Deserializes this object from a given cv::FileStorage.
+    virtual void load(const FileStorage& fs) = 0;
+
+    // Sets additional string info for the label
+    virtual void setLabelInfo(int label, const String& strInfo);
+
+    // Gets string info by label
+    virtual String getLabelInfo(int label);
+
+    // Gets labels by string info
+    virtual vector<int> getLabelsByString(const String& str);
+};
 
 class FacialRecognizer {
 public:
   // Facial Recognizer constructor
-  FacialRecognizer(cv::Mat matrix);
+  FacialRecognizer();
 
   void loadDataSet(){
 
@@ -35,7 +79,7 @@ public:
   }
 
   void createFaceRecognizer(int num_components=0, double threshold=DBL_MAX){
-
+    Ptr<FaceRecognizer> model = createEigenFaceRecognizer(num_components, threshold);
   }
 
 private:
