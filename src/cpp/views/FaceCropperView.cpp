1ad6e6bb32db19d4674ad1067cf4930b43a832d3
andusy <andus401@gmail.com>
2017-11-12 13:07:25 -0500

    Changed detect CropFace method

--- src/cpp/views/FaceCropperView.cpp
+++ src/cpp/views/FaceCropperView.cpp
@@ -7,7 +7,10 @@
 #include "views/FaceCropperView.h"
 
 std::string detectCropFace(Image& passThisImage) {
-  return cropFaceImageAsByteString(passThisImage);
+	cv::CascadeClassifier face; // Create new Cascade Classifier object
+	const std::string filePath = "data/haarcascade_frontalface_default.xml"; // Path of XML file
+	face = loadCascadeClassifier(filePath); // Load the CascadeClassifier object into face
+	return cropFaceImageAsByteString(passThisImage, face); // Crop the face
 }
 
 EMSCRIPTEN_BINDINGS(Turbo) {
