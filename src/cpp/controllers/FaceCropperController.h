f7dd134fb3dd3382933a1aa64fbd2b357358d463
andusy <andus401@gmail.com>
2017-11-12 12:20:17 -0500

    method header and comments

--- src/cpp/controllers/FaceCropperController.h
+++ src/cpp/controllers/FaceCropperController.h
@@ -7,7 +7,7 @@
 #include "models/Image.h"
 #include "opencv2/core.hpp"
 
-cv::CascadeClassifier loadCascadeClassifier(const std::string&);
-std::string cropFaceImageAsByteString(Image& passedImage);
+cv::CascadeClassifier loadCascadeClassifier(const std::string&); // Pre-load the cascade classifier from an xml file
+std::string cropFaceImageAsByteString(Image& passedImage, cv::CascadeClassifier); // Crops the image and returns it as a byte string
 
 #endif  // SRC_CPP_CONTROLLERS_FACECROPPERCONTROLLER_H_
