30d72a075ecd38a7760145b832ea965f10a4582d
Viraj Bangari <Viraj3f@gmail.com>
2017-11-19 03:14:20 -0500

    Automatically detect face

--- src/views/js/addCustomer.js
+++ src/views/js/addCustomer.js
@@ -4,8 +4,17 @@ let main = function() {
     Module.addCustomer_init();
     let photosTaken = false;
 
-    // Ran when the Take Picture button is clicked.
-    document.getElementById("tpButton").onclick = function(){
+    let displayMostRecentlyDetectedImage = function () {
+        let imageContainer = document.getElementById("images");
+        imageContainer.style.display = "flex";
+        let newImage = document.createElement("img");
+        newImage.src = "data:image/png;base64," + Module.addCustomer_mostRecentDetectedFace();
+        newImage.classList.add("croppedImage");
+        imageContainer.appendChild(newImage);
+    }
+
+    // Returns the webcam frame as a b64 string.
+    let getFrame = function () {
         let canvas = document.createElement('canvas');
         let ctx = canvas.getContext('2d');
 
@@ -15,26 +24,66 @@ let main = function() {
         //convert to desired file format
         let dataURI = canvas.toDataURL('image/png').replace('data:image/png;base64,','');
 
+        return dataURI;
+    }
+
+    // Ran when the Take Picture button is clicked.
+    document.getElementById("tpButton").onclick = function(){
+        let dataURI = getFrame();
+
         // Crop the face and save it to temporary storage.
         let success = Module.addCustomer_saveFaceInTemporaryStorage(dataURI);
         if (!success) {
             return;
         }
-
         photosTaken = true;
-        
-        // Display the updated faces.
-        let preview = document.getElementById("images");
-        preview.style.display = "flex";
-        let newImage = document.createElement("img");
-        newImage.src = "data:image/png;base64," + Module.addCustomer_mostRecentDetectedFace();
-        newImage.classList.add("croppedImage");
-        preview.appendChild(newImage);
+        displayMostRecentlyDetectedImage();
+    };
+
+    // Automatically detect faces
+    document.getElementById("detectFace").onclick = function(){
+        let imagesTaken = 0;
+        let imagesToTake = 8;
+        let delay = 100;
+
+        let singlePictureButton = document.getElementById("tpButton");
+        let saveButton = document.getElementById("save-button");
+        let clearButton = document.getElementById("clear-button");
+
+        this.disabled = true;
+        singlePictureButton.disabled = true;
+        saveButton.disabled = true;
+        clearButton.disabled = true;
+
+        let takePictureFromVideo = () => {
+            if (imagesTaken >= imagesToTake) {
+                this.disabled = false;
+                singlePictureButton.disabled = false;
+                saveButton.disabled = false;
+                clearButton.disabled = false;
+                return;
+            }
+
+            let dataURI = getFrame();
+
+            // Crop the face and save it to temporary storage.
+            let success = Module.addCustomer_saveFaceInTemporaryStorage(dataURI);
+            if (!success) {
+                return;
+            }
+
+            photosTaken = true;
+            displayMostRecentlyDetectedImage();
+
+            imagesTaken += 1;
+            setTimeout(takePictureFromVideo, 400);
+        }
+
+        takePictureFromVideo();
     };
 
     // Ran when the "save" button is clicked.
     document.getElementById("save-button").onclick = function() {
-        debugger;
         if (!photosTaken) {
             alert("No pictures taken - press the \"Take Picture\" button.")
             return;
@@ -47,12 +96,21 @@ let main = function() {
         }
         Module.addCustomer_clean();
         Module.persist().then(() => {
-            let nextURL = 
+            let nextURL =
                 window
                 .location
                 .href
                 .replace("addCustomer", "saveCustomer") + "?studentId=" + studentId;
             window.location.href = nextURL;
         });
+    }
+
+    document.getElementById("clear-button").onclick = function() {
+        Module.clearTemporaryStorage();
+        let imageContainer = document.getElementById("images");
+        while (imageContainer.hasChildNodes()) {
+            imageContainer.removeChild(imageContainer.lastChild);
+        }
+        imageContainer.style.display = 'none';
     }
 }
