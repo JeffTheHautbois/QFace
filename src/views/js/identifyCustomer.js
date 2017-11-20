8aca912230f1d3c6975ebeefcf339a50956f4029
Viraj Bangari <Viraj3f@gmail.com>
2017-11-20 13:59:34 -0500

    Add identifyCustomer html and js

---
+++ src/views/js/identifyCustomer.js
@@ -0,0 +1,110 @@
+// The code that runs after emscripten/db is fully loaded.
+let main = function() {
+    Module.addCustomer_init();
+    let photosTaken = false;
+
+    let displayMostRecentlyDetectedImage = function () {
+        let imageContainer = document.getElementById("images");
+        imageContainer.style.display = "flex";
+        let newImage = document.createElement("img");
+        newImage.src = "data:image/png;base64," + Module.addCustomer_mostRecentDetectedFace();
+        newImage.classList.add("croppedImage");
+        newImage.classList.add("fadeIn");
+        imageContainer.appendChild(newImage);
+    }
+
+    // Returns the webcam frame as a b64 string.
+    let getFrame = function () {
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext('2d');
+
+        //draw image to canvas. scale to target dimensions
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        //convert to desired file format
+        let dataURI = canvas.toDataURL('image/png').replace('data:image/png;base64,','');
+
+        return dataURI;
+    }
+
+    // Automatically detect faces
+    document.getElementById("detectFace").onclick = function(){
+        clearFaces();
+
+        let imagesTaken = 0;
+        let imagesToTake = 1;
+        let delay = 100;
+
+        let saveButton = document.getElementById("save-button");
+        let clearButton = document.getElementById("clear-button");
+
+        let imageCounter = document.getElementById("imageCounter")
+        let errorDisplay = document.getElementById("errorMessages")
+
+        this.disabled = true;
+        saveButton.disabled = true;
+        clearButton.disabled = true;
+        imageCounter.style.display = 'block';
+
+        // Display console log messages to user.
+        let oldLogger = window.console.log;
+        window.console.log = function(message) {
+            errorDisplay.innerText = message;
+        }
+
+        let takePictureFromVideo = () => {
+            if (imagesTaken >= imagesToTake) {
+                window.console.log = oldLogger;
+                this.disabled = false;
+                saveButton.disabled = false;
+                clearButton.disabled = false;
+                imageCounter.style.display = 'none';
+                return;
+            }
+
+            let dataURI = getFrame();
+
+            // Crop the face and save it to temporary storage.
+            let success = Module.addCustomer_saveFaceInTemporaryStorage(dataURI);
+
+            if (success) {
+                errorDisplay.innerText = "";
+                // Only record and display a picture when it was successfully
+                // detected.
+                photosTaken = true;
+                displayMostRecentlyDetectedImage();
+
+                imageCounter.innerText = "Images taken: " + imagesTaken + "/" + imagesToTake;
+                imagesTaken += 1;
+            }
+
+            setTimeout(takePictureFromVideo, 400);
+        }
+
+        takePictureFromVideo();
+    };
+
+    // Ran when the "save" button is clicked.
+    document.getElementById("save-button").onclick = function() {
+        Module.addCustomer_clean();
+        Module.persist().then(() => {
+            let nextURL =
+                window
+                .location
+                .href
+                .replace("identifyCustomer", "verify");
+            window.location.href = nextURL;
+        });
+    }
+
+    let clearFaces = function () {
+        Module.clearTemporaryStorage();
+        let imageContainer = document.getElementById("images");
+        while (imageContainer.hasChildNodes()) {
+            imageContainer.removeChild(imageContainer.lastChild);
+        }
+        imageContainer.style.display = 'none';
+    }
+
+    document.getElementById("clear-button").onclick = clearFaces();
+}
