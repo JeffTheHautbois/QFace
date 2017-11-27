571957b3ec269316570a1ef328fc1c4a0505e51b
Viraj Bangari <Viraj3f@gmail.com>
2017-11-27 12:56:23 -0500

    Clean up unused files

--- src/cpp/views/FaceCropperView.h
+++
@@ -1,9 +0,0 @@
-#ifndef SRC_CPP_VIEWS_FACECROPPERVIEW_H_
-#define SRC_CPP_VIEWS_FACECROPPERVIEW_H_
-
-#include <iostream>
-#include <string>
-
-void detectCropFace();
-
-#endif  // SRC_CPP_VIEWS_DUCKCROPPERVIEW_H_
